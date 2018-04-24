47b010a72d6e827b5529f5e309cba8a86a34411b
janachure <38654223+janachure@users.noreply.github.com>
2018-04-24 08:38:08 -0300

    Add files via upload

--- Lista.hpp
+++ Lista.hpp
@@ -34,14 +34,18 @@ template <typename T>
 void Lista<T>::agregarAdelante(const T& elem) {
     // Completar
     Nodo* nuevo= new Nodo;
-    if (_ultimo==NULL){   // COMO SI RECIEN LO ACABASTE DE CREAR
+    if (_ultimo==NULL){
         _ultimo=nuevo;
-    };
-    nuevo->_valor=elem;     // ASIGNA EL ELEMENTO PRIMERO
-    (*nuevo)._siguiente=_primero;   // EL SIGUIENTE CONECTALO CON EL PRIMERO
-    (*nuevo)._anterior=NULL;
-    _primero=nuevo;
-    //deberia andar
+        nuevo->_valor=elem;
+        nuevo->_siguiente=_primero;
+        nuevo->_anterior=NULL;
+    }else {
+        nuevo->_valor = elem;
+        nuevo->_siguiente=_primero;
+        nuevo->_anterior=NULL;
+        _primero->_anterior=nuevo;
+        _primero=nuevo;
+    }
 };
 
 template <typename T>
@@ -49,11 +53,11 @@ void Lista<T>::agregarAtras(const T& elem) {
     // Completar
     Nodo* nuevo= new Nodo;
     if (_primero==NULL){
-
+        _primero=nuevo;
     }
     (*nuevo)._valor=elem;
     (*nuevo)._anterior=_ultimo;
-    _ultimo=(*nuevo)._anterior;
+    _ultimo=nuevo;
     (*nuevo)._siguiente=NULL;
 };
 template <typename T>
@@ -71,13 +75,17 @@ int Lista<T>::longitud() const {
 template <typename T>
 const T& Lista<T>::iesimo(Nat i) const {
     // Completar
-    Nodo* senhala=_primero;
+    Nodo* nuevo= new Nodo;
+    nuevo=_primero;
     T resultado;
-    int iterador=0;
-    while (iterador!=longitud()+1){
-        iterador++;
-        resultado=senhala->_valor;
-        senhala= _primero->_anterior;
+    int contador=0;
+    while (nuevo->_siguiente!= NULL) {
+        if (contador==i) {
+            resultado = nuevo->_valor;
+            nuevo->_siguiente=NULL;
+        };
+        nuevo = nuevo-> _siguiente;
+        contador++;
     }
     return resultado;
 };
@@ -90,6 +98,19 @@ void Lista<T>::eliminar(Nat i) {
 template <typename T>
 T& Lista<T>::iesimo(Nat i) {
     // Completar (hint: es igual a la anterior...)
+    Nodo* nuevo= new Nodo;
+    nuevo=_primero;
+    T resultado;
+    int contador=0;
+    while (nuevo->_siguiente!= NULL) {
+        if (contador==i) {
+            resultado = nuevo->_valor;
+            nuevo->_siguiente=NULL;
+        };
+        nuevo = nuevo-> _siguiente;
+        contador++;
+    }
+    return resultado;
 }
 
 template <typename T>
